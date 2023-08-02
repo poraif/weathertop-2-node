fd14676fce5ff76809a71cf06fd75a068f12b54d
Glitch (weathertop-webdev2-por) <none>
2023-08-02 16:12:08 +0000

    adding and listing readings (lab05)

--- README.md
+++
@@ -1,3 +0,0 @@
-# WeatherTop
-
-This is an updated weatherTop app, built with Node, Handlebars and Express. WeatherTop is a companion app for the Weathertop 1000 weather station. 
---
+++ controllers/station-controller.js
@@ -0,0 +1,27 @@
+import { stationStore } from "../models/station-store.js";
+import { readingStore } from "../models/reading-store.js";
+
+export const stationController = {
+  async index(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const viewData = {
+      title: "Station",
+      station: station,
+    };
+    response.render("station-view", viewData);
+  },
+  
+    async addReading(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const newReading = {
+      code: request.body.code,
+      temperature: request.body.temperature,
+      windSpeed: request.body.windSpeed,
+      windDirection: request.body.windDirection,
+      pressure: request.body.pressure,
+    };
+    console.log(`adding reading ${newReading.code}`);
+    await readingStore.addReading(station._id, newReading);
+    response.redirect("/station/" + station._id);
+  },
+};
